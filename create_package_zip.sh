03269162a66a927c09e16db59e68b818f05a6efd
Thomas Kirk <kirk.thomas.d@gmail.com>
2023-07-24 20:28:56 -0400

    improve naming

--- create_package_zip.sh
+++ create_package_zip.sh
@@ -1,6 +1,6 @@
-mkdir package
-pip install --target package/ -r requirements.txt
-cd package
+mkdir deployment-package
+pip install --target deployment-package/ -r requirements.txt
+cd deployment-package
 zip -r ../colonist-deployment-package.zip .
 cd ..
 zip colonist-deployment-package.zip colonist-pii.yml main.py
