03e59ca0114f9b24dee863deb691da157391a0fc
Thomas Kirk <kirk.thomas.d@gmail.com>
2025-01-01 12:18:27 -0500

    some useful refactoring and hacky column addition script

--- game_cache.py
+++ game_cache.py
@@ -3,48 +3,55 @@ import json
 
 
 class GameCache:
-    def __init__(self, db_name=":memory:", primary_key_fields=None):
+    def __init__(self, db_name=":memory:", primary_key_fields=None, table_name="kv_store"):
         if primary_key_fields is None:
             primary_key_fields = ["username", "month"]
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
         self.primary_key_field_names = primary_key_fields
+        self.table_name = table_name
         self._create_table(primary_key_fields)
 
     def _create_table(self, primary_key_fields):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS kv_store (
-                {}
+        self.cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                {"".join(f"{key} TEXT, " for key in primary_key_fields)}
                 value TEXT,
-                PRIMARY KEY ({})
+                PRIMARY KEY ({",".join(primary_key_fields)})
             )
-        '''.format("".join(f"{key} TEXT, " for key in primary_key_fields), ",".join(primary_key_fields)))
+        ''')
         self.conn.commit()
 
     def set(self, primary_key_field_values, value):
         value_json = json.dumps(value)
         row_data = primary_key_field_values + [value_json]
         write_query = f'''
-            INSERT INTO kv_store ({",".join(self.primary_key_field_names + ["value"])})
+            INSERT INTO {self.table_name} ({",".join(self.primary_key_field_names + ["value"])})
             VALUES ({', '.join('?' for _ in row_data)})
             ON CONFLICT({",".join(self.primary_key_field_names)}) DO UPDATE SET value=excluded.value
         '''
         self.cursor.execute(write_query , [value for value in row_data])
         self.conn.commit()
 
-    def get(self, username, day):
-        self.cursor.execute('''
-            SELECT value FROM kv_store WHERE username=? AND day=?
-        ''', (username, day))
+    def get_values(self, username, day=None):
+        day_filter = "AND day=?" if day is not None else ""
+        params = [username, day]
+        self.cursor.execute(f'''
+            SELECT value FROM {self.table_name} WHERE username=? {day_filter}
+        ''', [param for param in params if param is not None])
+        values = self.cursor.fetchall()
+        return values
+
+    def get_rows(self, username):
+        self.cursor.execute(f'''
+            SELECT * FROM {self.table_name} WHERE username=?
+        ''', [username])
         rows = self.cursor.fetchall()
         return rows
-        # if rows:
-        #     return json.loads(row[0])
-        # return None
 
     def delete(self, username, day):
-        self.cursor.execute('''
-            DELETE FROM kv_store WHERE username=? AND day=?
+        self.cursor.execute(f'''
+            DELETE FROM {self.table_name} WHERE username=? AND day=?
         ''', (username, day))
         self.conn.commit()
 
--- local_analytics.py
+++ local_analytics.py
@@ -20,33 +20,44 @@ class ColonistDownloader:
             except yaml.YAMLError as exc:
                 self.logger.error(exc)
         self.username = self.pii['usernames'][1]['Tommy'][0]
-        self.primary_key_fields=["username", "day", "game_id"]
+        self.primary_key_fields=["username", "day", "game_id", "rank"]
         self.db = GameCache(db_name="colonist_history.db", primary_key_fields=self.primary_key_fields)
 
     def download_games(self):
         url = ColonistDownloader.colonist_history_url.format(self.username)
         response = requests.get(url)
         response.raise_for_status()
-        print(response.status_code)
         games = response.json()['gameDatas']
         for game in games:
-            game_date = format_date_from_millis(game["startTime"])
-            self.db.set([self.username, game_date, game["id"]], game)
+            game_date, rank, _ = self.extract_values(game)
+            self.db.set([self.username, game_date, game["id"], rank], game)
 
-    def read_game(self):
-        today = datetime.today().strftime("%Y-%m-%d")
-        games = self.db.get(self.username, today)
-        games_stats = [self.transform_game(game) for game in games]
+    def read_game(self, date):
+        games = self.db.get_values(self.username, date)
+        games_stats = [self.extract_values(json.loads(game[0])) for game in games]
         for i, game in enumerate(games_stats):
-            print(f"Game {i+1} result: {'won' if game[0] else 'lost'} in {game[1]} minutes")
-        print(f"Spent {sum(time for result, time in games_stats)} minutes playing catan today.")
+            print(f"Game {i+1} result: {'won' if game[1]==1 else 'lost'} in {game[2]} minutes")
+        print(f"Spent {sum(time for _, result, time in games_stats)} minutes playing catan today.")
 
-    def transform_game(self, game):
-        game_json = json.loads(game[0])
-        won = next(player['rank']==1 for player in game_json['players'] if player['username'] == self.username)
-        print(won)
-        duration = int(game_json['duration']) // 1000 / 60
-        return won, duration
+    def extract_values(self, game):
+        rank = next(player['rank'] for player in game['players'] if player['username'] == self.username)
+        duration = int(game['duration']) // 1000 / 60
+        game_date = format_date_from_millis(game["startTime"])
+        return game_date, rank, duration
+
+    def run_schema_update(self):
+        rows = self.db.get_rows(self.username)
+        updated_rows = 0
+        for row in rows:
+            if row[3] is not None:
+                continue
+            else:
+                game = json.loads(row[4])
+                game_date, rank, _ = self.extract_values(game)
+                self.db.set([self.username, game_date, game["id"], rank], game)
+                updated_rows += 1
+        print(f"updated {updated_rows} rows")
+
 
 
 def format_date_from_millis(milliseconds):
@@ -58,7 +69,8 @@ def main():
     # AWS SES client
     tracker = ColonistDownloader()
     tracker.download_games()
-    tracker.read_game()
+    tracker.read_game(datetime.today().strftime("%Y-%m-%d"))
+    # tracker.run_schema_update()
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
